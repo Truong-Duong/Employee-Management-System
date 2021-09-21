9c23be8ada0b798a4427528e45b98ca460b2b51c
Truong Duong <truong.duong1908@gmail.com>
2021-09-21 17:42:37 -0500

    working on the mySQL

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,29 @@
-INSERT INTO departments (name)
-VALUES ("Sales"),
-       ("Engineering"),
-       ("Finance"),
-       ("Legal");
+INSERT INTO departments (department_name)
+VALUES  ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("Software Engineer", 100000, 2),
-       ("Accountant", 80000, 3),
-       ("Lawyer", 200000, 4),
-       ("Salesperson", 150000, 1),
-       ("Sales Lead", 300000, 1);
+VALUES  ("Salesperson", 35000, 1),
+        ("Sales Lead", 80000, 1);
+        ("Software Engineer", 160000, 2),
+        ("Mechanical Engineer", 60000, 2),
+        ("Accountant", 80000, 3),
+        ("Financial Analyst", 112000,3),
+        ("Lawyer", 240000, 4),
+        ("Judge", 360000, 4),
+
+        
 
 INSERT INTO employees (first_name, last_name, role_id)
-VALUES ("Alexa", "Mathis", 1),
-       ("Haris", "Franco", 2),
-       ("Kathryn", "Adamson", 3),
-       ("Miles", "Lancaster", 4),
-       ("Darcey", "Frey", 5);
+VALUES  ("Matt", "Matsil", 1),
+        ("Anika", "Krieger", 1),  
+        ("Anna", "Kretsinger", 2),
+        ("Ari", "Drake", 2),
+        ("Caroline", "Heuer", 3),
+        ("Julia", "Cunningham", 3),
+        ("Katherine", "Deloney", 4),
+        ("Kiley", "Pittman", 4),
+        ("Sarah", "Pinkerton", 5);
+        ("Katie", "Barlow", 5);
